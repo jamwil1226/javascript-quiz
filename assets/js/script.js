3409d09f13e751c7178ad2bd02d29cd760450afb
Jamie Williams <jamwil1226@yahoo.com>
2021-09-18 21:21:34 -0400

    debugged javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -191,7 +191,7 @@ function allDone() {
             var newScore = JSON.stringify(allScores);
             localStorage.setItem("allScores", newScore);
             // move to final page
-            window.location.replace("./HighScores.html");
+            window.location.replace("./highscores.html");
         }
     });
 
